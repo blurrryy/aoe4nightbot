9c70fe6e5e6b1cf5c16004ca2d038a11837047b3
Daniel Rienas <blurrryy@gmail.com>
2021-11-24 21:22:27 +0100

    First Overlay Test

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
 const express = require("express");
 const cors = require("cors");
-const moment = require('moment');
-moment.locale('de');
+const moment = require("moment");
+moment.locale("de");
 
 const app = express();
 app.use(cors());
@@ -18,6 +18,49 @@ const refreshData = async () => {
 
 refreshData();
 setInterval(refreshData, 1000 * 60 * 60 * 24);
+
+const HTML_OVERLAY_TOP = `
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="refresh" content="30">
+    <style>
+    @font-face {
+        font-family: Belmont;
+        src: url(/static/Belmont_Regular.otf);
+    }
+    * {
+        font-family: 'Belmont';
+    }
+
+    .player1 {
+        font-size: 25px;
+        color: rgb(0, 0, 255);
+    }
+
+    .player2 {
+        font-size: 25px;
+        color: red;
+    }
+
+    .titlesTop {
+        font-size: 30px;
+        color: white;
+        text-decoration: underline;
+    }
+    .titlesBot {
+        font-size: 25px;
+        color: white;
+        text-decoration: overline;
+    }
+    </style>
+</head><body>`
+
+const HTML_OVERLAY_BOT = `
+</html></body>`
 
 app.get("/rank", async (req, res) => {
   let params = {
@@ -78,20 +121,82 @@ app.get("/match", async (req, res) => {
       mapType = mapType ? mapType.string : null;
       let playerStrings = [];
 
-      for(let p of data.players) {
-          let civ = globalData.civ.find((x) => x.id === p.civ);
-          civ = civ ? civ.string : null;
-          playerStrings.push(`${p.name} (${p.rating})${civ? ` mit ${civ}`:''}`)
+      for (let p of data.players) {
+        let civ = globalData.civ.find((x) => x.id === p.civ);
+        civ = civ ? civ.string : null;
+        playerStrings.push(
+          `${p.name} (${p.rating})${civ ? ` mit ${civ}` : ""}`
+        );
       }
-      let dateString = data.started ? moment(new Date(parseInt(data.started)*1000).toISOString()).fromNow() : null
-      playerStrings = playerStrings.join(' gegen ')
-      let outputString = `Aktuellstes Match: ${playerStrings} ${mapType?`auf ${mapType}`:''} ${dateString?`gestartet ${dateString}`:''}`
+      let dateString = data.started
+        ? moment(
+            new Date(parseInt(data.started) * 1000).toISOString()
+          ).fromNow()
+        : null;
+      playerStrings = playerStrings.join(" gegen ");
+      let outputString = `Aktuellstes Match: ${playerStrings} ${
+        mapType ? `auf ${mapType}` : ""
+      } ${dateString ? `gestartet ${dateString}` : ""}`;
 
-      res.send(outputString)
+      res.send(outputString);
     }
   } catch (err) {
     console.error(new Error(err));
     res.send(`Ups! Fehler bei der Schnittstellenanfrage!`);
+  }
+});
+
+app.use('/static',express.static('files'));
+
+app.get("/overlay", async (req, res) => {
+  let params = {
+    game: "aoe4",
+    count: 1,
+    steam_id: "76561198012752362",
+  };
+
+  const sendStyle = (code = '') => {
+      return `${HTML_OVERLAY_TOP}${code}${HTML_OVERLAY_BOT}`
+  }
+
+  try {
+    let data = await axios.get(`https://aoeiv.net/api/player/matches`, {
+      params,
+    });
+    if (data.status !== 200) throw `Ups! Fehler bei der Schnittstellenanfrage!`;
+    data = data.data;
+    if (data.length < 1) {
+      res.send(sendStyle());
+    } else {
+      data = data[0];
+      if (data.num_players > 2) {
+        res.send(sendStyle());
+        return;
+      }
+      let mapType = globalData.map_type.find((x) => x.id === data.map_type);
+      mapType = mapType ? mapType.string : '';
+      let players = [];
+      for (let p of data.players) {
+        let civ = globalData.civ.find((x) => x.id === p.civ);
+        civ = civ ? civ.string : null;
+        players.push(
+          `(${p.rating}) ${p.name} | ${civ ? `${civ.substring(0,3)}` : ""}`
+        );
+      }
+
+      res.send(sendStyle(`
+        <body>
+            <span class="titlesTop">Aktuellstes Spiel</span><br>
+            <span class="player1">${players[0]}</span><br>
+            <span class="player2">${players[1]}</span><br>
+            <span class="titlesBot">${mapType}</span><br>
+        </body>
+        </html>      
+      `));
+    }
+  } catch (err) {
+    console.error(new Error(err));
+    res.send(sendStyle());
   }
 });
 
